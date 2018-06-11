71f91fac4c04b512855493a20516ed7418e37104
Chuanbo Pan <panchuanbo@yahoo.com>
2018-06-11 02:23:41 -0700

    [music257] added lots of packages + board movement

---
+++ final-proj-unity/Assets/_Main/Scripts/GM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+    public GameObject cameraRig;
+
+	// Use this for initialization
+	void Start () {
+        // UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 oldPos = cameraRig.transform.position;
+        cameraRig.transform.position = new Vector3(oldPos.x + 0.010f, oldPos.y, oldPos.z);
+	}
+}
